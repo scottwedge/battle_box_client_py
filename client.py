2d9d7778c031525a8e43f4b82412ce77786153ce
swedge <scott.wedge@gmail.com>
2020-04-12 21:55:08 -0400

    Fixes #1

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ def send_message(socket, msg):
     header = struct.pack("!H", len(msg_bytes))
     socket.sendall(header + msg_bytes)
 
-def recieve_message(socket):
+def receive_message(socket):
     msg_size_bytes = socket.recv(2)
     (msg_size,) = struct.unpack("!H", msg_size_bytes)
     message = socket.recv(msg_size)
@@ -27,17 +27,17 @@ connection = socket.create_connection((HOST, PORT))
 socket = context.wrap_socket(connection, server_hostname=HOST)
 
 send_message(socket, {'token': TOKEN, 'lobby': LOBBY})
-msg = recieve_message(socket)
+msg = receive_message(socket)
 connection_message = json.loads(msg)
 print(repr(connection_message))
 print("connection_id:", connection_message["connection_id"])
 
 send_message(socket, {'action': 'start_match_making'})
-msg = recieve_message(socket)
+msg = receive_message(socket)
 status = json.loads(msg)
 print("status:", status["status"])
 
-msg = recieve_message(socket)
+msg = receive_message(socket)
 decoded = json.loads(msg)
 print("Recieved Request:", decoded["request_type"])
 print("game_id:", decoded["game_info"]["game_id"])
@@ -46,12 +46,12 @@ print("player:", decoded["game_info"]["player"])
 send_message(socket, {"action": "accept_game", "game_id": decoded["game_info"]["game_id"]})
 
 for i in range(0, 100):
-    msg = recieve_message(socket)
+    msg = receive_message(socket)
     decoded = json.loads(msg)
     print(i, ": Recieved Request:", decoded["request_type"], "commands_request_id", decoded["commands_request"]["request_id"])
     send_message(socket, {"action": "send_commands", "request_id": decoded["commands_request"]["request_id"], "commands": []})
 
-msg = recieve_message(socket)
+msg = receive_message(socket)
 decoded = json.loads(msg)
 print("Game Over:", decoded["info"])
 print("Result: ", decoded["result"]["score"])
